4698bca9aa31218e7c14d03c7163d622cabdb898
Leejieun <ji88192@gmail.com>
2023-04-09 15:08:36 +0900

    init

---
+++ src/main/java/com/godcoder/mytome/MytomeApplication.java
@@ -0,0 +1,13 @@
+package com.godcoder.mytome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/godcoder/mytome/MytomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godcoder.mytome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
